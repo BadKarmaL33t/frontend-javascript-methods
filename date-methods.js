2f3576f8a77537f8c80747cb9e8ab6d5ce9250c2
BadKarmaL33t <jchagesteijn@protonmail.com>
2023-06-28 16:50:04 +0200

    opdrachten uitwerken

--- date-methods.js
+++ date-methods.js
@@ -5,7 +5,6 @@
 const currentDay = new Date();
 console.log(currentDay);
 
-
 /* Opdracht 1 */
 // 1a. Maak een nieuw date-object aan voor dit huidige moment en log het aantal uren in de terminal
 // 1b. Log daarna het aantal minuten in de terminal
@@ -13,8 +12,26 @@ console.log(currentDay);
 // Uren: 16 (als er een vier in de klok zit op het moment van aanroepen)
 // Minuten: 36 (als het 16:36 is op het moment van aanroepen)
 
+const timeStamp = new Date('June 28, 2023, 16:30:00');
 
+console.log(timeStamp.getHours());
+console.log(timeStamp.getMinutes())
 
 /* Opdracht 2 */
 // Format het date-object dat je bij de vorige vraag hebt gemaakt naar een leesbare datum
 // ---- Verwachte uitkomst: Tue Apr 06 2021 (afhankelijk van het moment)
+
+simpleTimeStamp = timeStamp.toDateString();
+
+console.log(simpleTimeStamp);
+
+const optionsNL = {
+    weekday: 'short',
+    year: 'numeric',
+    month: 'long',
+    day: 'numeric',
+};
+
+dutchTimeStamp = timeStamp.toLocaleDateString("nl-NL", optionsNL);
+
+console.log(dutchTimeStamp);
